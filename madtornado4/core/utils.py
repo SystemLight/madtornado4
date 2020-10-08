dadc323b3fa39a1ee6a4f839cb7fd8347e3efc16
SystemLight <1466335092@qq.com>
2020-10-08 12:51:29 +0800

    [2020-10-08]添加kill_from_port工具函数

--- madtornado4/core/utils.py
+++ madtornado4/core/utils.py
@@ -1,5 +1,6 @@
+import os
 import json
-
+import platform
 from typing import List, Optional, Callable, TypeVar, Iterable, Tuple
 
 
@@ -210,3 +211,20 @@ class UpdateList(list):
         for index, item in enumerate(self):
             if callback(item):
                 return index, item
+
+
+def kill_form_port(port):
+    """
+
+    根据传入端口号，尝试杀死进程，支持windows和linux平台
+
+    :param port: 端口号，int类型
+    :return: None
+
+    """
+    port = str(port)
+    if platform.system() == 'Windows':
+        command = """for /f "tokens=5" %i in ('netstat -ano ^| find \"""" + port + """\" ') do (taskkill /f /pid %i)"""
+    else:
+        command = """kill -9 $(netstat -nlp | grep :""" + port + """ | awk '{print $7}' | awk -F "/" '{print $1}')"""
+    os.system(command)
